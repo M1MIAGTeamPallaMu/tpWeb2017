a8c3b2bc276c85a40ad91d0b4a8f44feab5d9551
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-12 14:06:06 +0100

    updated interaction.js

--- interaction.js
+++ interaction.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var DnD = /** @class */ (function () {
     function DnD(canvas, interactor) {
-        this._canvas = canvas;
-        this._interactor = interactor;
+        this.canvas = canvas;
+        this.interactor = interactor;
     }
     DnD.prototype.initCoordinates = function () {
         this.xInit = 0;
@@ -12,28 +12,68 @@ var DnD = /** @class */ (function () {
         this.yFinal = 0;
     };
     DnD.prototype.select = function (evt) {
-        this.xInit = DnD.getMousePosition(this._canvas, evt).x;
-        this.yInit = DnD.getMousePosition(this._canvas, evt).y;
-        this._pressed = true;
+        this.xInit = DnD.getMousePosition(this.canvas, evt).newX;
+        this.yInit = DnD.getMousePosition(this.canvas, evt).newY;
+        this.pressed = true;
     };
     DnD.prototype.move = function (evt) {
         if (this._pressed) {
-            this.xFinal = DnD.getMousePosition(this._canvas, evt).x;
-            this.yFinal = DnD.getMousePosition(this._canvas, evt).y;
+            this._xFinal = DnD.getMousePosition(this.canvas, evt).newX;
+            this._yFinal = DnD.getMousePosition(this.canvas, evt).newY;
         }
     };
     DnD.prototype.release = function (evt) {
-        this.xFinal = DnD.getMousePosition(this._canvas, evt).x;
-        this.yFinal = DnD.getMousePosition(this._canvas, evt).y;
-        this._pressed = false;
+        this.xFinal = DnD.getMousePosition(this.canvas, evt).newX;
+        this.yFinal = DnD.getMousePosition(this.canvas, evt).newY;
+        this.pressed = false;
     };
     DnD.getMousePosition = function (canvas, evt) {
         var rect = canvas.getBoundingClientRect();
         return {
-            x: evt.x - rect.left,
-            y: evt.y - rect.top
+            newX: evt.x - rect.left,
+            newY: evt.y - rect.top
         };
     };
+    Object.defineProperty(DnD.prototype, "yFinal", {
+        get: function () {
+            return this._yFinal;
+        },
+        set: function (value) {
+            this._yFinal = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(DnD.prototype, "xFinal", {
+        get: function () {
+            return this._xFinal;
+        },
+        set: function (value) {
+            this._xFinal = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(DnD.prototype, "yInit", {
+        get: function () {
+            return this._yInit;
+        },
+        set: function (value) {
+            this._yInit = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(DnD.prototype, "xInit", {
+        get: function () {
+            return this._xInit;
+        },
+        set: function (value) {
+            this._xInit = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Object.defineProperty(DnD.prototype, "canvas", {
         get: function () {
             return this._canvas;
@@ -67,4 +107,3 @@ var DnD = /** @class */ (function () {
     return DnD;
 }());
 exports.DnD = DnD;
-//# sourceMappingURL=interaction.js.map
